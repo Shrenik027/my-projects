624dd2d73a389b79d4799ac89a8486f343011c9a
Shrenik <shrenik9477@gmail.com>
2023-11-15 15:51:33 +0000

    Upload New File

---
+++ Cprogramming/program5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int x = 10;
+int y;
+
+void office() {
+
+	printf("in office nusta coadding......\n");
+
+}
+
+void main(){
+
+	printf("wake-up\n");
+	office();
+	printf("sleep\n");
+
+
+}
