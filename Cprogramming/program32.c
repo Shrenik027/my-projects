f4d5869e76681087881f340c19ac3c49373c4d90
Shrenik <shrenik9477@gmail.com>
2023-11-18 15:36:41 +0000

    Nov 18

---
+++ Cprogramming/program32.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main(){
+
+	int numofstud =350;
+	float sal = 70.50;
+	char ch = 'A';
+
+	int __age = 30;
+	float company15 = 75.50;
+	//char 1val ='Z'; -----> error
+        
+	int C2W =1000;
+	char val$Data ='W';
+	//int -xyz =70  ------> error
+}
