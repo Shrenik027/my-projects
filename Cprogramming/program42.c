c7010a95995e2b7ef06f64a80584ed52d4494f8a
Shrenik <shrenik9477@gmail.com>
2023-11-19 04:13:39 +0000

    Nov 19

---
+++ Cprogramming/program42.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main(){
+
+	int x;
+	printf("Enter value");
+	scanf("%d",&x);
+
+	if(x%2==0 ){
+		printf("X is divisable by 2\n");
+	}
+	else{
+		printf("X is not divisible by 2\n");
+	
+	}
+		       	
+			
+			
+
+}
