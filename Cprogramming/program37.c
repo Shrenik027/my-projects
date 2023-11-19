52287fa0090496975a89029a772e8299fe4bef1d
Shrenik <shrenik9477@gmail.com>
2023-11-19 04:12:05 +0000

    nov 19

---
+++ Cprogramming/program37.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main(){
+
+	printf("Strat MAin\n");
+	int x =0;
+	int y=20;
+
+	if (x){
+		printf("IN first if block");
+	}
+
+	if (y){
+		printf("in second if bolck");
+	
+	}
+}
