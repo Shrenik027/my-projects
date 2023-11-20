6aba432ee23b0547242fb118ed534db048818d72
Shrenik <shrenik9477@gmail.com>
2023-11-20 16:31:47 +0000

    Nov 20

---
+++ Cprogramming/program51.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+void main(){
+
+        int x;
+        printf("enter Value : ");
+        scanf("%d",&x);
+
+        switch(x){
+
+              
+                case '65':
+                        printf("x=65\n");
+                        break;
+                case '66':
+                        printf("x=66\n");
+                        break;
+                case '67':
+                        printf("x=67\n");
+                        break;
+                case '68':
+                        printf("x=68\n");
+                        break;
+                default:
+                        printf("wrong input\n");
+
+        }
+
+}
