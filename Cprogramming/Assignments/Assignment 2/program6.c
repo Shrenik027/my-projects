63274b2127f1ea2d73da2bfb5422509eb778353b
Shrenik <shrenik9477@gmail.com>
2023-11-25 08:18:39 +0000

    Upload New File

---
+++ Cprogramming/Assignments/Assignment 2/program6.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+void main(){
+	int a;
+	printf("Enter Number1: ");
+	scanf("%d",&a);
+
+	int b;
+        printf("Enter Number2: ");
+        scanf("%d",&b);
+
+	int c;
+        printf("Enter Number3: ");
+        scanf("%d",&c);
+
+	int d;
+        printf("Enter Number4: ");
+        scanf("%d",&d);
+
+	int e;
+        printf("Enter Number5: ");
+        scanf("%d",&e);
+
+	int f;
+        printf("Enter Number6: ");
+        scanf("%d",&f);
+
+	int g;
+        printf("Enter Number7: ");
+        scanf("%d",&g);
+
+	int h;
+        printf("Enter Number8: ");
+        scanf("%d",&h);
+
+	int i;
+        printf("Enter Number9: ");
+        scanf("%d",&i);
+
+	int j;
+        printf("Enter Number10: ");
+        scanf("%d",&j);
+
+	if (a,b,c,d,e,f,g,h,i,j){
+		int sum;
+		sum=a+b+c+d+e+f+g+h+i+j;
+		printf("Sum Is: ");
+		printf("%d\n",sum);
+
+		float avg;
+		avg=(a+b+c+d+e+f+g+h+i+j)/10;
+		printf("Avg Is: ");
+                printf("%f\n",avg);
+
+	}
+
+}
