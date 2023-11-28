2db38a70e53219711d0c224adf51cdea7ce64a97
Shrenik <shrenik9477@gmail.com>
2023-11-28 14:44:09 +0000

    Replace program2.c

--- Cprogramming/Assignments/Assignment4/program2.c
+++ Cprogramming/Assignments/Assignment4/program2.c
@@ -5,18 +5,14 @@ void main(){
 	int y=10;
 
 	while(x==1,y==10){
-		for( x;x<=10;){
-			printf("%d",x);
-			printf("+");
-			break;
-			x++;
-		}
-		for( y;y>=1;){
-			printf("%d",y);
-			printf("=");
-			printf("%d\n",x+y);
-			break;
-			y--;
+		for(int i=x;i<=y;){
+			for(y;y>=x;y--){
+				int sum;
+				sum=i+y;
+				printf("%d + %d = %d\n",i,y,sum);
+				i++;
+			}
+	                 
 		}
 	}
 
