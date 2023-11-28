7debdc3df757117c41489e4092d3e8062e94ec79
Shrenik <shrenik9477@gmail.com>
2023-11-28 15:36:27 +0000

    Replace program4.c

--- Cprogramming/Assignments/Assignment4/program4.c
+++ Cprogramming/Assignments/Assignment4/program4.c
@@ -5,7 +5,15 @@ void main(){
 	printf("Enter A Number: ");
 	scanf("%d",&x);
 
-	if(x%x==0 && x%1==0){
-		printf("this is prime no\n");
+	for(int i=2;i<=x;i++){
+		if(x%x==0 && x%i!=0){
+			printf("%d is prime number",x);
+			break;
+		}
+	        else{
+			printf("%d is Not a Prime Number",x);
+			break;
+		}	
+		
 	}
 }
