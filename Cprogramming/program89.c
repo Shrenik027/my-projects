8ac1d984827f0f1b4a74446107950ed91f958a15
Shrenik <shrenik9477@gmail.com>
2023-11-30 15:45:58 +0000

    Upload New File

---
+++ Cprogramming/program89.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+void main(){
+        int input;
+        int x=2;
+        int y=3;
+        printf("Enter number");
+        scanf("%d",&input);
+
+        switch(input){
+                case 5:
+                        printf("5\n");
+                        break;
+                /*case x+y:
+                        printf("2+3\n");
+                        break;
+                case y+x:
+                        printf("3+2\n");
+                        break;*/
+                default:
+                        printf("default\n");
+
+
+
+
+
+
+
+        }}
