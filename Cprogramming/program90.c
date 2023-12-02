6cc73975f740372363335d31938795de81496e8e
Shrenik <shrenik9477@gmail.com>
2023-12-02 13:43:23 +0000

    Upload New File

---
+++ Cprogramming/program90.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main(){
+
+        printf("Strat MAin\n");
+        char ch1='A';
+        char ch2=48;
+
+        if (ch1){
+                printf("IN first if block\n");
+        }
+
+        if (ch2){
+                printf("in second if bolck\n");
+
+        }
+        printf("End Main\n");
+}
