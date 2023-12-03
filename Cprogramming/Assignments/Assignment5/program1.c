601b34a161815580aba167ee4affc349891be405
Shrenik <shrenik9477@gmail.com>
2023-12-03 14:51:32 +0000

    Upload New File

---
+++ Cprogramming/Assignments/Assignment5/program1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main(){
+	int a;
+	for (int i=1;i<=4;i++){
+		a=i;
+
+		for(int j=1; j<=4;j++){
+
+     			printf("%d ",a);
+			a++;
+		}
+                
+		printf("\n");
+		
+		
+
+	}
+}
