3a648aec6552790e66649806935f3cc33c25776a
Shrenik <shrenik9477@gmail.com>
2023-12-03 14:52:59 +0000

    Upload New File

---
+++ Cprogramming/Assignments/Assignment5/program9.c
@@ -0,0 +1,22 @@
+/*#include <stdio.h>
+
+int main() {
+    int rows, i, j, value = 2;
+
+    // Input the number of rows
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    // Loop to iterate through each row
+    for (i = 0; i < rows; i++) {
+        // Loop to print elements in each row
+        for (j = 0; j < rows; j++) {
+            printf("%-4d", value);
+            value += 2 * (i + 1) + j * 3; // Adding consecutive odd numbers
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+*/
