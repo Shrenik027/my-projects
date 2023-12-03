cdc99fb84d3c16226eed5a63212f202b871a10e3
Shrenik <shrenik9477@gmail.com>
2023-12-03 14:52:50 +0000

    Upload New File

---
+++ Cprogramming/Assignments/Assignment5/program8.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int rows, i, j, value = 1;
+
+   
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    
+    for (i = 0; i < rows; i++) {
+        
+        for (j = 0; j < rows; j++) {
+            printf("%-4d", value);
+            value += 2;
+        }
+        printf("\n");
+    }
+
+    
+}
+
