347903b3784023a179a5b723ea12f87a6708a471
Shrenik <shrenik9477@gmail.com>
2023-12-04 13:27:43 +0000

    Upload New File

---
+++ Cprogramming/For -{Pattern Problems}/pattern1/program11.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void main(){
+
+	int rows;
+	printf("Enter Rows: ");
+	scanf("%d",&rows);
+
+	int x=1;
+	char ch='a';
+
+	for(int i=1;i<=rows;i++){
+		for(int j=1;j<=rows;j++){
+			if(i%2!=0){
+				printf("%d ",x);
+				x++;
+			}
+			else{
+				printf("%c ",ch);
+				ch++;
+			}
+		}
+		printf("\n");
+	}
+}
