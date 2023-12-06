ec636373d9b9cf1d27765f12029956617cb3a2d4
Shrenik <shrenik9477@gmail.com>
2023-12-06 15:53:09 +0000

    Upload New File

---
+++ Cprogramming/For -{Pattern Problems}/simple nested for loop 2/program3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main(){
+	int x=14;
+        for( int i=1;i<=4;i++ ){
+                
+
+                for( int j=1;j<=4;j++){
+                        printf("%d ",x);
+                        x++;
+                }
+                x=14;
+		x=x+i;
+                printf("\n");
+        }
+}
