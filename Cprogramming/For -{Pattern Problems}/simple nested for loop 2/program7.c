c4343cb46eb6ebf48e79fcd7f7b2e8130c40d5ff
Shrenik <shrenik9477@gmail.com>
2023-12-06 15:53:55 +0000

    Upload New File

---
+++ Cprogramming/For -{Pattern Problems}/simple nested for loop 2/program7.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void main(){
+
+
+        int x=1;
+        int a=3;
+        for( int i=1;i<=3;i++ ){
+
+
+                for( int j=1;j<=3;j++){
+			printf("%d ",x);
+			x=x+a;
+			if(x%4==0){
+				x=(x^1/2);
+			}
+			a=a+2;
+
+
+                }
+
+                printf("\n");
+        }
+}
