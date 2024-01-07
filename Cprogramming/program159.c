24dbf7245fcbcb3fcd66dea6799a016d2de7432d
Shrenik <shrenik9477@gmail.com>
2024-01-07 14:54:49 +0000

    Add new file

---
+++ Cprogramming/program159.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+void main()
+{
+
+	
+	int arr[5] = { 15, 25, 35, 45, 55 };
+
+	printf("Element at arr[2]: %d\n", arr[2]);
+
+
+	printf("Element at arr[4]: %d\n", arr[4]);
+
+
+	printf("Element at arr[0]: %d", arr[0]);
+
+
+
+}
