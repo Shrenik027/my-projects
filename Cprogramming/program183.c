217f4b0a091f8aaa7d27d97dc32d454b7e966fa6
Shrenik <shrenik9477@gmail.com>
2024-01-11 16:43:45 +0000

    Upload New File

---
+++ Cprogramming/program183.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void main(){
+
+	float package1;
+	float package2;
+	float package3;
+	float package4;
+	float package5;
+
+	printf("Enter Packagees:\n");
+	scanf("%f",&package1);
+	scanf("%f",&package2);
+	scanf("%f",&package3);
+	scanf("%f",&package4);
+	scanf("%f",&package5);
+
+	printf("Friend 1 Package:%f\n",package1);
+	printf("Friend 2 Package:%f\n",package2);
+	printf("Friend 3 Package:%f\n",package3);
+	printf("Friend 4 Package:%f\n",package4);
+	printf("Friend 5 Package:%f\n",package5);
+}
