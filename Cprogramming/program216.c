389b30a76a4bb046b28e6bd7058353e256acc696
shrenik9477 <shrenik9477@gmail.com>
2024-01-13 14:05:44 +0530

    13 Jan

---
+++ Cprogramming/program216.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+void main(){
+
+	//int arr[]; --> array size missing
+	
+
+        int arr[4];
+
+        arr[0]=10;
+        arr[1]=20;
+        arr[2]=30;
+        arr[3]=40;
+
+	printf("%d\n",arr[0]);
+	printf("%d\n",arr[1]);
+	printf("%d\n",arr[2]);
+	printf("%d\n",arr[3]);
+
+	
+
+      // char arr1[4]={'A','B','C','D'};
+
+       
+	printf("%c\n",arr[0]);
+	printf("%c\n",arr[1]);
+	printf("%c\n",arr[2]);
+	printf("%c\n",arr[3]);
+
+	
+}
