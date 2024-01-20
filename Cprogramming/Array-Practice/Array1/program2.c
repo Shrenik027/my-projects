600735e631ba711a1eaa2436aec1ed528edc4717
shrenik9477 <shrenik9477@gmail.com>
2024-01-20 10:11:58 +0530

    20 Jan

---
+++ Cprogramming/Array-Practice/Array1/program2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+void main(){
+
+        int size;
+
+        printf("Enter Size of arry:\n");
+        scanf("%d",&size);
+
+        int arr[size];
+
+        printf("Enter Elements:\n");
+
+        for (int row=0;row<size;row++){
+
+                scanf("%d",&arr[row]);
+
+        }
+        printf("array Elements are:\n");
+
+
+        for (int row=0;row<size;row++){
+
+                printf("%d \n",arr[row]);
+
+        }
+
+        printf("Even Elements are:\n");
+	int Epdt=1;
+
+
+        for (int row=0;row<size;row++){
+                if(arr[row]%2==0){
+
+                printf("%d \n",arr[row]);
+		Epdt=Epdt*arr[row];
+                }
+
+        }
+	printf("Product Of Even Numbers:");
+	printf("%d\n ",Epdt);
+
+}
